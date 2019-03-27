94582f5d08e9642a5ea06e00c7d27999d57031e2
Alexey Elymanov <strangeqargo@gmail.com>
2019-03-27 11:30:53 +0300

    removing debug trash

--- zsh_trap.sh
+++ zsh_trap.sh
@@ -46,7 +46,7 @@ preexec () {
     else
         echo TAG: $HISTORY_TAG
     fi;
-		echo "cl ${COMP_LINE}"
+	
     [ -n "$COMP_LINE" ] && return  # do nothing if completing
     #[ "$BASH_COMMAND" = "$PROMPT_COMMAND" ] && return # don't cause a preexec for $PROMPT_COMMAND
 
